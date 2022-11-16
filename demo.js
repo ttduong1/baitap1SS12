b812992b91b5fee128086ae02cbf7173d5952095
duongt <trinhtungduong2019@gmail.com>
2022-11-16 14:42:31 +0700

    update

---
+++ demo.js
@@ -0,0 +1,98 @@
+// function bangcuuchuong()
+// {
+//     document.write("<table border=1 width=100%>")
+//     for(i=1;i<=9;i++)
+//     {
+//         document.write("<tr>")
+//         for(n=2;n<=9;n++)
+//         document.write("<td>" +n + "x" + i +"="+ (n*1));
+//         document.write("</td>");
+//         document.write("</tr>")
+//     }
+//     document.write("</tr></table>")
+// }
+
+// n=prompt("Nhập");
+// if(n==2)
+
+{document.write("<font color=black><br>Bảng cửu chương 2:");
+document.write("<font color=black>");
+	
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 2 = ",+2*j);
+			}
+		
+}
+// if(n==3)
+{document.write("<font color=black><br>Bảng cửu chương 3:");
+ document.write("<font color=black>");
+	
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>"," 3 x ",j,"=",+3*j);
+			}
+		
+}
+// if(n==4)
+{document.write("<font color=black><br>Bảng cửu chương 4:");
+ document.write("<font color=black>");
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 4 = ",+4*j);
+			}
+		
+}
+// if(n==5)
+{document.write("<font color=black><br>Bảng cửu chương 5:");
+ document.write("<font color=black>");
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 5 = ",+5*j);
+			}
+		
+}
+// else
+// if(n==6)
+{document.write("<font color=black><br>Bảng cửu chương 6:");
+ document.write("<font color=black>");
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 6 = ",+6*j);
+			}
+		
+}
+// if(n==7)
+{document.write("<font color=black><br>Bảng cửu chương 7:");
+ document.write("<font color=black>");
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 7 = ",+7*j);
+			}
+		
+}
+// if(n==8)
+{document.write("<font color=black><br>Bảng cửu chương 8:");
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 8 = ",+8*j);
+			}
+		
+}
+// if(n==9)
+{document.write("<font color=black><br>Bảng cửu chương 9:</font>");
+document.write("<font color=black>");
+			for(j=1;j<=10;j++)
+			{
+			
+			document.write("<br>",j," x 9 = ",9*j);
+			}
+		
+}
